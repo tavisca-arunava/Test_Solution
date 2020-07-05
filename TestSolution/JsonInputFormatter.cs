f3dadf7a0ed85e6a89925d00734d7713664417b2
tavisca-arunava <41727957+tavisca-arunava@users.noreply.github.com>
2020-07-05 14:23:09 +0530

    first commit

---
+++ TestSolution/JsonInputFormatter.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.Formatters;
+using Tavisca.Platform.Common.Profiling;
+namespace TestSolution
+{
+    public class JsonInputFormatter : InputFormatter
+    {
+        public JsonInputFormatter(Microsoft.AspNetCore.Mvc.Formatters.JsonInputFormatter inner)
+        {
+            _inner = inner;
+        }
+
+        Microsoft.AspNetCore.Mvc.Formatters.JsonInputFormatter _inner;
+
+        public override async Task<InputFormatterResult> ReadRequestBodyAsync(InputFormatterContext context)
+        {
+            using (new ProfileContext("Json Input Formatter - ReadRequestBodyAsync"))
+            {
+                return await _inner.ReadRequestBodyAsync(context);
+            }
+        }
+
+        public override bool CanRead(InputFormatterContext context)
+        {
+            return _inner.CanRead(context);
+        }
+
+        public override IReadOnlyList<string> GetSupportedContentTypes(string contentType, Type objectType)
+        {
+            return _inner.GetSupportedContentTypes(contentType, objectType);
+        }
+
+        public override Task<InputFormatterResult> ReadAsync(InputFormatterContext context)
+        {
+            using (new ProfileContext("input_formatter - Json Input Formatter - ReadAsync"))
+            {
+                return _inner.ReadAsync(context);
+            }
+        }
+    }
+}
---
+++ TestSolution/Startup.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+
+namespace TestSolution
+{
+    public class Startup
+    {
+        public Startup(IHostingEnvironment env)
+        {
+            var builder = new ConfigurationBuilder()
+                            .SetBasePath(env.ContentRootPath)
+                            .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                            .AddJsonFile($"appsettings.{env.EnvironmentName}.json", optional: true)
+                            .AddEnvironmentVariables();
+
+
+            Configuration = builder.Build();
+            Environment.SetEnvironmentVariable("ASPNETCORE_ENVIRONMENT", env.EnvironmentName);
+            _environmentName = env.EnvironmentName;
+            Console.WriteLine("Processor count for Web Application: {0}", Environment.ProcessorCount);
+        }
+
+        private readonly string _environmentName;
+        private bool _isCompressionEnabled;
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            ThreadPool.SetMinThreads(200, 200);
+
+        }
+
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseMvc();
+        }
+    }
+}
